55f976c1eeda16f79f32482e720b6bf10fa5155f
冯罡 <1353640486@qq.com>
2019-11-17 11:24:25 +0800

    transfrom to functional component

--- src/renderer/views/main/recommend/index/index.vue
+++ src/renderer/views/main/recommend/index/index.vue
@@ -1,55 +1,3 @@
-<template>
-  <v-container fluid class="container-1040">
-    <v-row>
-      <base-swiper :list="banners" />
-    </v-row>
-    <base-title text="推荐歌单" to="/main/recommend/song-list" />
-    <v-row class="d-flex justify-space-between">
-      <div class="daily-wrapper" :style="{width: '18.46%'}">
-        <div class="inner">
-          <div class="day grey--text text--darken-2">
-            {{day}}
-          </div>
-          <div class="date">
-            {{date}}
-          </div>
-        </div>
-        <div class="name">每日歌曲推荐</div>
-      </div>
-      <base-song-list-cover
-        v-for="songList in recommendSongList"
-        :key="songList.id"
-        width="18.46%"
-        :song-list="songList"
-      />
-    </v-row>
-    <base-title text="独家放送"  to='/main/personalizedContent'/>
-    <v-row class="d-flex justify-space-between mb-9" v-if="personalizedContent.length">
-      <base-personalized-content-cover v-for="content in personalizedContent" :key="content.videoId"
-      width="32.21%" :personalized-content="content"/>
-    </v-row>
-    <base-title text="最新音乐"  to='/main/recommend/latest-music'/>
-    <v-row class="border-a  mb-9">
-      <v-col class="pa-0 border-r" cols="6">
-        <BaseLatestMusicItem v-for="(music, index) in latestMusic.slice(0, 5)" :key="music.id" :music='music' :index='index + 1' :stripe="index % 2 === 1"/>
-      </v-col>
-      <v-col class="pa-0" cols="6">
-        <BaseLatestMusicItem v-for="(music, index) in latestMusic.slice(5, 10)" :key="music.id" :music='music' :index='index + 6' :stripe="index % 2 === 1"/>
-      </v-col>
-    </v-row>
-    <base-title text="推荐MV" to='/main/video/index'/>
-    <v-row class="d-flex justify-space-between">
-      <base-video-cover v-for="mv in recommendMV" :key='mv.id' :video='mv' width="24.03%"/>
-    </v-row>
-    <base-title text="主播电台" to="/main/recommend/radio" />
-    <v-row class="d-flex justify-space-between">
-      <BaseRadioCover v-for="radio in recommendRadio" :key="radio.id" :radio='radio' width="15.38%"/>
-    </v-row>
-    <v-row>
-    </v-row>
-  </v-container>
-</template>
-
 <script>
 import BaseSwiper from '@/base/swiper/base-swiper.vue'
 import BaseTitle from '@/base/title/base-title.vue'
@@ -58,7 +6,6 @@ import BasePersonalizedContentCover from '@/base/personalized-content-cover/base
 import BaseLatestMusicItem from '@/base/latest-music-item/base-latest-music-item.vue'
 import BaseVideoCover from '@/base/video-cover/base-video-cover.vue'
 import BaseRadioCover from '@/base/radio-cover/base-radio-cover.vue'
-import Func from '@/base/func.vue'
 import dayjs from '@/common/day.js'
 
 import {
@@ -90,6 +37,7 @@ export default {
   },
   data() {
     return {
+      layout: ['推荐歌单','独家放送','最新音乐', '推荐MV', '主播电台'],
       day: dayjs().format('dddd'),
       date: dayjs().format('D'),
       banners: [],
@@ -117,6 +65,131 @@ export default {
     })
     getRecommendMV().then(data => this.recommendMV = data.result)
     getRecommendRadio().then(data => this.recommendRadio = data.djRadios)
+  },
+  methods:{
+    genDynamicTemplate(tempName){
+      const isTempNameValid = ['推荐歌单','独家放送','最新音乐', '推荐MV', '主播电台'].indexOf(tempName) !== -1
+      if(isTempNameValid) {
+        if(tempName === '推荐歌单') {
+          return (
+            <div>
+              <base-title text="推荐歌单" to="/main/recommend/song-list" />
+              <v-row class="d-flex justify-space-between">
+                <div class="daily-wrapper" style={{width: '18.46%'}}>
+                  <div class="inner">
+                    <div class="day grey--text text--darken-2">
+                      {this.day}
+                    </div>
+                    <div class="date">
+                      {this.date}
+                    </div>
+                  </div>
+                  <div class="name">每日歌曲推荐</div>
+                </div>
+                {
+                  this.recommendSongList.map(songList => {
+                    return (<base-song-list-cover
+                      key={songList.id}
+                      width="18.46%"
+                      song-list={songList}
+                    />)
+                  })
+                }
+              </v-row>
+            </div>
+          )
+        }
+        if(tempName === '独家放送') {
+          return (
+            <div>
+              <base-title text="独家放送"  to='/main/personalizedContent'/>
+              <v-row class="d-flex justify-space-between mb-9">
+                {
+                  this.personalizedContent.map(content => {
+                    return (
+                      <base-personalized-content-cover key={content.videoId}
+                        width="32.21%" personalized-content={content}/>
+                    )
+                  })
+                }
+              </v-row>
+            </div>
+          )
+        }
+        if(tempName === '最新音乐') {
+          return (
+            <div>
+              <base-title text="最新音乐"  to='/main/recommend/latest-music'/>
+              <v-row class="border-a  mb-9">
+                <v-col class="pa-0 border-r" cols="6">
+                  {
+                    this.latestMusic.slice(0, 5).map((music, index) => {
+                      return (
+                        <BaseLatestMusicItem  key={music.id} music={music} index={index + 1} stripe={index % 2 === 1}/>
+                      )
+                    })
+                  }
+                </v-col>
+                <v-col class="pa-0" cols="6">
+                  {
+                    this.latestMusic.slice(5, 10).map((music, index) => {
+                      return (
+                        <BaseLatestMusicItem  key={music.id} music={music} index={index + 6} stripe={index % 2 === 1}/>
+                      )
+                    })
+                  }
+                </v-col>
+              </v-row>
+            </div>
+          )
+        }
+        if(tempName === '推荐MV') {
+          return (
+            <div>
+              <base-title text="推荐MV" to='/main/video/index'/>
+              <v-row class="d-flex justify-space-between">
+                {
+                  this.recommendMV.map(mv => {
+                    return (
+                      <base-video-cover  key={mv.id} video={mv} width="24.03%"/>
+                    )
+                  })
+                }
+      
+              </v-row>
+            </div>
+          )
+        }
+        if(tempName === '主播电台') {
+          return (
+            <div>
+              <base-title text="主播电台" to="/main/recommend/radio" />
+              <v-row class="d-flex justify-space-between">
+                {
+                  this.recommendRadio.map(radio => {
+                    return <BaseRadioCover key={radio.id} radio={radio} width="15.38%"/>
+                  })
+                }
+              </v-row>
+            </div>
+          )
+        }
+      }
+    }
+  },
+  render(){
+    return (
+      <v-container fluid class="container-1040">
+        <v-row>
+          <base-swiper list={this.banners} />
+        </v-row>
+        {
+          this.layout.map(tempName => this.genDynamicTemplate(tempName))
+        }
+        <v-row>
+        </v-row>
+      </v-container>
+    )
   }
 }
 </script>
@@ -127,31 +200,33 @@ export default {
   flex-direction: column;
   overflow: hidden;
   margin-bottom: 36px;
-  .inner{
+  .inner {
     width: 100%;
     padding-top: 100%;
     position: relative;
     cursor: pointer;
     margin-bottom: 8px;
     background-color: #fff;
-    .day,.date{
+    .day,
+    .date {
       position: absolute;
     }
-    .day{
+    .day {
       top: 10%;
       font-size: 16px;
       text-align: center;
       width: 100%;
     }
-    .date{
+    .date {
       top: 10%;
       width: 100%;
       text-align: center;
       font-size: 104px;
       color: #b82525;
-      font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-      @media screen and (max-width: 1066px){
-        font-size: 96px
+      font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+        "Lucida Sans", Arial, sans-serif;
+      @media screen and (max-width: 1066px) {
+        font-size: 96px;
       }
     }
   }
