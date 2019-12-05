963e79f0faadecd8cc1afdb232830c0fc9752b9b
冯罡 <1353640486@qq.com>
2019-12-05 17:27:27 +0800

    update

--- src/renderer/API/video.js
+++ src/renderer/API/video.js
@@ -28,6 +28,7 @@ export const getVideoByTag = id => axios.get('/video/group', {params: {id, _t: M
 
 **可选参数 :**   
 `area`: 地区,可选值为全部,内地,港台,欧美,日本,韩国,不填则为全部
+
 `type`: 类型,可选值为全部,官方版,原生,现场版,网易出品,不填则为全部  
 
 `order`: 排序,可选值为上升最快,最热,最新,不填则为上升最快  
