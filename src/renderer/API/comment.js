1e0b88ac45ed4cc7778f2f414b2d011b73cf01b2
冯罡 <1353640486@qq.com>
2019-12-21 21:27:05 +0800

    视频评论

--- src/renderer/API/comment.js
+++ src/renderer/API/comment.js
@@ -56,3 +56,26 @@ export const getSongListComment = ({id, limit = 20, page = 1, before}) => axios.
 */
 
 export const getHotComments = ({id, type, limit = 20, page = 1, before}) => axios.get('/comment/hot', {params: {id, type, limit, page, offset: (page - 1) * limit ,before}})
+
+
+
+/*
+### 视频评论
+
+说明 : 调用此接口 , 传入音乐 id 和 limit 参数 , 可获得该 视频 的所有评论 (
+不需要登录 )
+
+**必选参数 :** `id`: 视频的 id
+
+**可选参数 :** `limit`: 取出评论数量 , 默认为 20
+
+`offset`: 偏移数量 , 用于分页 , 如 :( 评论页数 -1)\*20, 其中 20 为 limit 的值  
+
+`before`: 分页参数,取上一页最后一项的 `time` 获取下一页数据(获取超过5000条评论的时候需要用到)
+
+**接口地址 :** `/comment/video`
+
+**调用例子 :** `/comment/video?id=89ADDE33C0AAE8EC14B99F6750DB954D`
+*/
+
+export const getVideoComments = ({id, page = 1, limit = 50}) => axios.get('/comment/video', {params: {id, offset: (page - 1) * limit, limit,}})
--- src/renderer/views/main/video-detail/video-detail.vue
+++ src/renderer/views/main/video-detail/video-detail.vue
@@ -1,49 +1,91 @@
 <template>
   <v-container fluid class="container-1040 d-flex" v-if="video">
-    <div class="left mr-4 ">
+    <div class="left mr-7 pr-4">
       <v-row class="flex align-center">
-        <i class="iconfont icon-return pl-1 pr-1" @click="$router.back()"></i>
+        <i class="iconfont icon-return pr-1" @click="$router.back()"></i>
         <div class="subtitle-1 text-ellipsis">{{video.title}}</div>
         <div class="caption ml-4">by {{video.creator.nickname}}</div>
       </v-row>
-      <div class="mt-1">
+      <v-row class="mt-1">
         <div ref="player"></div>
-      </div>
+      </v-row>
+      <v-row class="mt-3 mb-12">
+        <v-btn color="#25272b" height="26px" class="subtitle-3 pl-2 pr-2">
+          <i class="iconfont icon-praise grey--text mr-1"></i>
+          赞({{video.praisedCount}})
+        </v-btn>
+        <v-btn color="#25272b" height="26px" class="subtitle-3 pl-2 pr-2 ml-3 mr-3">
+          <i class="iconfont icon-addfile grey--text mr-1"></i>
+          收藏({{video.subscribeCount}})
+        </v-btn>
+        <v-btn color="#25272b" height="26px" class="subtitle-3 pl-2 pr-2">
+          <i class="iconfont icon-share grey--text mr-1"></i>
+          分享({{video.shareCount}})
+        </v-btn>
+      </v-row>
+      <base-title text="评论" />
+      <v-container fluid class="pl-0 pr-0">
+        <base-comment-input class="mb-4"/>
+        <base-title text="精彩评论" v-if="page === 1 && hotComments.length !== 0"/>
+        <v-row v-if="page === 1 && hotComments.length !== 0">
+          <base-comment-item v-for="comment in hotComments" :key="comment.commentId" :comment="comment"/>
+        </v-row>
+        <v-btn block text v-if="page === 1 && hotComments.length !== 0 && moreHot" class="mt-2 mb-4"
+        @click='$router.push({name: "hot-comments", params: {type: 5, id}})'
+        >查看更多精彩评论 <i class="iconfont icon-enter" ></i></v-btn>
+        <base-title text="最新评论"/>
+        <v-row>
+          <base-comment-item v-for="comment in comments" :key="comment.commentId" :comment="comment"/>
+        </v-row>
+        <v-row>
+          <v-pagination v-if="video && video.commentCount > 50" v-model="page" total-visible="9" :length="Math.floor(video.commentCount / 50)" color="#b82525"/>
+        </v-row>
+      </v-container>
     </div>
-    <div class="right ml-4">
+    <div class="right pl-4">
       <base-title text="视频介绍" />
       <v-row class="d-flex justify-space-between mb-4">
         <span class="caption grey--text text--darken-1">发布时间 {{video.publishTime}}</span>
         <span class="caption grey--text text--darken-1">播放次数 {{video.playTime}}</span>
       </v-row>
       <v-row>
-        <div class="caption grey--text text--darken-1">
-          标签: {{video.videoGroup.map(i => i.name).join(' / ')}}
-        </div>
+        <div class="caption grey--text text--darken-1">标签: {{video.videoGroup.map(i => i.name).join(' / ')}}</div>
       </v-row>
     </div>
   </v-container>
 </template>
 
 <script>
-import {getVideoData, getVideoPlayURL} from '@/API/video.js'
+import { getVideoData, getVideoPlayURL } from '@/API/video.js'
+import {getVideoComments} from '@/API/comment.js'
+import BaseCommentInput from '@/base/comment-input/base-comment-input.vue'
 import BaseTitle from '@/base/title/base-title.vue'
+import BaseCommentItem from '@/base/comment-item/base-comment-item.vue'
 import dayjs from '@/common/day.js'
 import Player from 'xgplayer'
 export default {
   data: () => ({
     id: '',
-    video: null
+    video: null,
+    page: 1,
+    comments: [],
+    hotComments: [],
+    moreHot: false
   }),
   components: {
-    BaseTitle
+    BaseTitle,
+    BaseCommentInput,
+    BaseCommentItem
   },
-  created(){
+  created() {
     this.id = this.$route.params.id
-    getVideoPlayURL(this.id).then(({urls}) => {
+    getVideoPlayURL(this.id).then(({ urls }) => {
       const [source] = urls
-      getVideoData(this.id).then(({data: video}) => {
-        this.video = {...video, ...{publishTime: dayjs(video.publishTime).format('YYYY-MM-DD')}}
+      getVideoData(this.id).then(({ data: video }) => {
+        this.video = {
+          ...video,
+          ...{ publishTime: dayjs(video.publishTime).format('YYYY-MM-DD') }
+        }
         this.$nextTick(() => {
           new Player({
             el: this.$refs['player'],
@@ -55,27 +97,46 @@ export default {
         })
       })
     })
+    this.getVideoComments()
   },
   methods: {
-    initVideo(){
-
+    getVideoComments(){
+      getVideoComments({id: this.id, page: this.page}).then(({comments, hotComments, moreHot}) => {
+        this.comments = comments
+        this.hotComments = hotComments
+        this.moreHot = moreHot
+      })
+    }
+  },
+  watch: {
+    page(){
+      this.getVideoComments()
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.icon-return{
+.icon-return {
   font-size: 20px;
   cursor: pointer;
 }
-.left{
-  width: 700px;
-  .title{
+.left {
+  width: 686px;
+  .title {
     max-width: 60%;
   }
 }
-.right{
-  width: calc(100% - 720px);
+.right {
+  width: 294px;
+}
+.icon-praise,.icon-praise_fill{
+  font-size: 20px;
+}
+.icon-addfile {
+  font-size: 20px;
+}
+.icon-share {
+  transform: translateY(1px);
 }
 </style>
